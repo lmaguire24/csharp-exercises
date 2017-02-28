8ebe6536dc1fb00a751e781c3165024698d6235b
lmaguire24 <lenon.maguire@gmail.com>
2017-02-27 17:58:54 -0800

    New programs

---
+++ Area/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            string input = Console.ReadLine();
+            float radius = System.Convert.ToInt32(input);
+            var area = 3.14 * (radius * radius);
+            Console.WriteLine("The area of your circle is " + area);
+            Console.ReadLine();
+        }
+    }
+}
